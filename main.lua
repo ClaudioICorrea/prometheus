b0eef33c1b2c798021cd0d139c54c58e7cbb2e8b
Hezen-Dar <gallardovera.d@gmail.com>
2022-10-06 14:47:00 -0300

    Menú centrado, correciónes menores

--- main.lua
+++ main.lua
@@ -6,17 +6,19 @@ require "projectiles"
 require "Utilities"
 player = Object:extend()
 bullets ={}
+screen_width = love.graphics.getWidth() --ancho de la ventana
+screen_height = love.graphics.getHeight() --alto de la ventana
 --CLASES---
 --Clase jugador--luke es muy bonito
-function player:new(x, y, target_x, target_y, velocity, clik_right, click_left,radius)
+function player:new(x, y, target_x, target_y, velocity, click_right, click_left,radius)
     self.radius = radius or 30
     self.velocity = velocity or 0
     self.x = x or 0
     self.y = y or 0
     self.target_x = target_x or 0
     self.target_y = target_y or 0
-    self.clik_right = clik_right or false
-    self.clik_left = click_left or false 
+    self.click_right = click_right or false
+    self.click_left = click_left or false
 end
 
 
@@ -59,11 +61,11 @@ end
 
 function love.mousepressed(x, y, button, istouch, presses)
     if not game.state['running'] then
-        if button == 1 then 
-            if game.state["menu"] then 
+        if button == 1 then
+            if game.state["menu"] then
                 for index in  pairs(buttons.menu_states) do
                     buttons.menu_states[index]:checkPressed(x, y , player1.radius)
-                end 
+                end
             end
         end
     end
@@ -71,20 +73,20 @@ end
 --Other Methods--hola apu
 -- enemy --
 function enemy:move_to(location_x,location_y)
-    direction ={(location_x-self.x )/norm(self.x-location_x,self.y-location_y),(location_y-self.y)/norm(self.x-location_x,self.y-location_y)} 
+    direction ={(location_x-self.x )/norm(self.x-location_x,self.y-location_y),(location_y-self.y)/norm(self.x-location_x,self.y-location_y)}
     self.x = self.x + direction[1]*self.velocity
     self.y = self.y + direction[2]*self.velocity
-end    
+end
 
 --cargar
 function love.load()
     --jugador---
     player1 = player(750,600,50,50,5,5)
     --menu--
-    buttons.menu_states.play_game = newButton("Play Game", StartGame , nil, 110, 50) 
-    buttons.menu_states.setting = newButton("Setting", nil, nil, 110, 50) 
-    buttons.menu_states.exit_game = newButton("Exit Game", love.event.quit, nil, 110, 50) 
-    buttons.menu_states.restart_game = newButton("Re-start", nil, nil, 110, 50) 
+    buttons.menu_states.play_game = newButton("Play Game", StartGame , nil, 110, 50)
+    buttons.menu_states.setting = newButton("Setting", nil, nil, 110, 50)
+    buttons.menu_states.exit_game = newButton("Exit Game", love.event.quit, nil, 110, 50)
+    buttons.menu_states.restart_game = newButton("Re-start", nil, nil, 110, 50)
     --esenario--
     --enemigos y npc--
     enemies_mele = {}
@@ -96,8 +98,8 @@ function love.load()
 end
 
 function love.update()
-    --movimientos del jugador-- 
-    if game.state["running"] then  
+    --movimientos del jugador--
+    if game.state["running"] then
         if love.keyboard.isDown("w") then
             player1.y = player1.y - 1*player1.velocity
         end
@@ -116,7 +118,7 @@ function love.update()
         end
     end
     player1.target_x = love.mouse.getX() -- ubicacion del mouse en x
-    player1.target_y = love.mouse.getY() -- ubicacion del mouse en y 
+    player1.target_y = love.mouse.getY() -- ubicacion del mouse en y
     player1.click_right = love.mouse.isDown(1)
     player1.click_left = love.mouse.isDown(2)
 
@@ -146,16 +148,16 @@ function love.update()
         end
     end
 end
---dibujar 
-function love.draw() 
-    if game.state["running"] then 
+--dibujar
+function love.draw()
+    if game.state["running"] then
     -- Dibujar Jugador 1 --
-        love.graphics.setColor(360, 360, 360) --dibujar en el color indicado 
+        love.graphics.setColor(360, 360, 360) --dibujar en el color indicado
         square_draw("fill", player1.x , player1.y ,20,20)
-        love.graphics.setColor(360, 360, 360) --dibujar en el color indicado 
+        love.graphics.setColor(360, 360, 360) --dibujar en el color indicado
         if player1.click_right then
             love.graphics.setColor(1, 0, 0)
-        end    
+        end
         if player1.click_left then
             love.graphics.setColor(0, 1, 0)
         end
@@ -171,11 +173,11 @@ function love.draw()
     for i,bullet in ipairs(bullets) do
         bullet:draw()
     end
-    elseif game.state["menu"] then 
-        love.graphics.print("MENU", 70,10)
-        buttons.menu_states.play_game:draw(40, 30 ,20 ,20)
-        buttons.menu_states.setting:draw(40, 90 ,20 ,20)
-        buttons.menu_states.exit_game:draw(40, 150,20 ,20)
-        buttons.menu_states.restart_game:draw(40, 210 ,20 ,20)
+    elseif game.state["menu"] then
+        love.graphics.print("MENU", screen_width / 2 - 25, 10)
+        buttons.menu_states.play_game:draw(screen_width / 2 - 55, screen_height / 7 * 1, 20, 20)
+        buttons.menu_states.setting:draw(screen_width / 2 - 55, screen_height / 7 * 2, 20, 20)
+        buttons.menu_states.exit_game:draw(screen_width / 2 - 55, screen_height / 7 * 3, 20, 20)
+        buttons.menu_states.restart_game:draw(screen_width / 2 - 55, screen_height / 7 * 4, 20, 20)
     end
 end
