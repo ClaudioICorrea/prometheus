35985ddecaeccb58503a8669c4e5b08e02a5cb14
Bastian <vulpinoaxis@gmail.com>
2022-10-08 20:00:05 -0300

    más features añadidas al boton

--- Buttons2.lua
+++ Buttons2.lua
@@ -1,7 +1,7 @@
 Object = require "lukeclassic"
 Button = Object:extend()
 
-function Button:new(text, func, param, width, height, x_pos, y_pos)
+function Button:new(text, func, param, width, height, x_pos, y_pos, font)
     self.width = width or 100
     self.height = height or 100
     self.func = func or function() print("Este Boton no tiene funcionalidad") end
@@ -9,8 +9,13 @@ function Button:new(text, func, param, width, height, x_pos, y_pos)
     self.text = text or "sin texto"
     self.button_x = x_pos or 0
     self.button_y = y_pos or 0
-    self.text_x = 0
-    self.text_y = 0
+    self.font = font or love.graphics.getFont()
+    text = love.graphics.newText(self.font, self.text)
+    text_width = text:getWidth()
+    text_height = text:getHeight()
+    self.text_x = (self.width - text_width)/2
+    self.text_y = (self.height - text_height)/2
+
 end
 
 function Button:_check_click()
@@ -24,7 +29,12 @@ function Button:_check_click()
 end
 
 function Button:draw()
-    love.graphics.rectangle('line', self.button_x, self.button_y, self.width, self.height)
+
+    love.graphics.setColor(0.6, 0.6, 0.6)
+    love.graphics.rectangle("fill", self.button_x, self.button_y, self.width, self.height)
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.print(self.text, self.button_x + self.text_x, self.button_y +self.text_y)
+    love.graphics.setColor(1, 1, 1)
 end
 
 
